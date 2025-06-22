1a0d552a0c1a0d1b5d69f217af4760a4b72525f9
Rafael Zimmer <rzimmerdev@gmail.com>
2025-06-22 18:57:37 -0300

    Executor works on iterators

--- dxlib/strategy/executor.py
+++ dxlib/strategy/executor.py
@@ -1,4 +1,4 @@
-from typing import Type, Union
+from typing import Type, Union, Generator, Iterator
 
 from dxlib import History
 from .history_view import HistoryView
@@ -8,16 +8,26 @@ class Executor:
     def __init__(self, strategy):
         self.strategy = strategy
 
-    def run(self, origin: History, history_view: Union[Type[HistoryView], HistoryView]):
-        observer = history_view.iter(origin)
+    def run(self,
+            origin: History | Iterator[History],
+            history_view: Union[Type[HistoryView], HistoryView],
+            history: History = None,
+            ):
+        observer = history_view.iter(origin) if isinstance(origin, History) else origin
 
-        if (observation := next(observer, None)) is None:
-            return History(history_schema=self.strategy.output_schema(origin))
+        observation = None
 
-        history = observation.copy()
-        result = History(history_schema=self.strategy.output_schema(observation)).concat(
-            self.strategy.execute(observation, history, history_view)
-        )
+        if history is None:
+            if (observation := next(observer, None)) is None:
+                return History(history_schema=self.strategy.output_schema(origin))
+            history = observation.copy()
+
+        result = History(history_schema=self.strategy.output_schema(history))
+
+        if observation is not None:
+            result = result.concat(
+                self.strategy.execute(observation, history, history_view)
+            )
 
         for observation in observer:
             history.concat(observation)
